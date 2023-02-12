3d377a8cbc8e21b5280194071180be3135441fef
OrenKotSalomon <orensalomonkot@gmail.com>
2023-02-12 13:48:25 +0200

    added basic layout and logo

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
-    <h1>METABIT</h1>
+    <div class="logo-container">
+      <img src="../assets/imgs/logo.png" alt="" />
+      <h1>METABIT</h1>
+    </div>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/car">Cars</RouterLink>
